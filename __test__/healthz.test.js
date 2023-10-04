2e0a377de1fb881fed96fde76fa53f5636506c8a
nihil27011996 <pellissery.n@northeastern.edu>
2023-10-04 08:41:46 -0400

    added the installing of postgresql

--- __test__/healthz.test.js
+++ __test__/healthz.test.js
@@ -1,9 +1,13 @@
-import startServer from '../server.js'
-import supertest from 'supertest'
+import supertest from 'supertest';
+import app, { startServer } from '../server.js';
 
-describe('health endpoint',() => {
+describe('health endpoint', () => {
+    beforeAll(() => {
+        startServer(); // Start the server before running tests
+    });
+
     it('success criteria return 200 ok', async () => {
-        const responsVar = await supertest(startServer).get('/healthz');
+        const response = await supertest(app).get('/healthz');
         expect(response.status).toBe(200);
-    })
+    });
 });
