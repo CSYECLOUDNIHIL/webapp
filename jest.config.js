14a1d7994312c0e63738f6cddd8d9fb8c5e0755a
nihil27011996 <pellissery.n@northeastern.edu>
2023-10-04 08:45:42 -0400

    added the installing of postgresql

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 export default {
     testEnvironment: 'node', // Set the test environment to Node.js
+    transform: {
+        '^.+\\.js$': 'babel-jest', // Use Babel to transpile ES6 modules
+      },
     testMatch: ['**/*.test.js'], // Specify the test file pattern
     collectCoverage: true, // Enable code coverage reports
     coverageDirectory: 'coverage', // Specify the directory for coverage reports
