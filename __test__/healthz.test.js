8945495a300fb2e430bc879bb8fc9d069cc57eff
nihil27011996 <pellissery.n@northeastern.edu>
2023-10-04 10:03:27 -0400

    added the installing of postgresql

--- __test__/healthz.test.js
+++ __test__/healthz.test.js
@@ -7,11 +7,6 @@ describe('health endpoint', () => {
   beforeAll(async () => {
     server = await startServer();
   });
-
-  afterAll(async () => {
-    await server.close();
-  });
-
   it('success criteria return 200 ok', async () => {
     const response = await supertest(server).get('/healthz');
     expect(response.status).toBe(200);
