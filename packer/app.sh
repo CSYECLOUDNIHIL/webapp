7285554818b0a1745a8f87db5667860c9ac85b07
nihil27011996 <114561628+nihil27011996@users.noreply.github.com>
2023-10-18 01:51:19 -0400

    Feature branch (#33)
    * commenting the yaml files
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * testing the build
    
    * git pushing the app.sh file
    
    * commiting the app.sh script
    
    * commiting the app.sh script
    
    * commiting the app.sh script

--- packer/app.sh
+++ packer/app.sh
@@ -4,14 +4,21 @@ sudo apt-get update
 sudo apt-get install -y nodejs npm
 node -v
 npm -v
-
-cd /home/admin/
-
 sudo apt-get install -y unzip
 
-
-
 mkdir webapp-main
+
+ls -a
+
+mv webapp.zip ./webapp-main
+
+if [ -f /home/admin/webapp-main/webapp.zip ]; then
+    echo "webapp.zip exists"
+else
+    echo "webapp.zip does not exist"
+fi
+
+
 
 cd webapp-main
 
