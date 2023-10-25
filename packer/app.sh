cd541d83a44cbc39a6afbeac05445a2f871827ae
nihil27011996 <114561628+nihil27011996@users.noreply.github.com>
2023-10-25 18:02:27 -0400

    Feature branch (#61)
    * to create a new ami
    
    * removed services
    
    * removed services
    
    * adding the create user
    
    * adding the create user
    
    * changes to the code

--- packer/app.sh
+++ packer/app.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y nodejs npm
 sudo apt-get install -y unzip
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
-unzip webapp.zip -d csye6225
+unzip webapp.zip -d /opt/csye6225
 sudo cp /opt/csye6225/packer/healthz-systemd.service /lib/systemd/system/healthz-systemd.service
 npm install
 sudo apt-get remove -y git
