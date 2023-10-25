e1385678e6ce89483a137579ae066d41886b8d76
nihil27011996 <pellissery.n@northeastern.edu>
2023-10-25 17:32:16 -0400

    adding the create user

--- packer/app.sh
+++ packer/app.sh
@@ -3,12 +3,9 @@ sudo apt-get update
 sudo apt-get install -y nodejs npm
 sudo apt-get install -y unzip
 sudo groupadd csye6225
-sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225 
-mkdir webapp-main
-mv webapp.zip ./webapp-main
-cd webapp-main
-unzip webapp.zip
-sudo cp /home/admin/webapp/Packer/healthz-systemd.service /lib/systemd/system/healthz-systemd.service
+sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+unzip webapp.zip -d csye6225
+sudo cp /opt/csye6225/packer/healthz-systemd.service /lib/systemd/system/healthz-systemd.service
 npm install
 sudo apt-get remove -y git
 sudo apt-get clean
