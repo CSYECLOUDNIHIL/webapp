1e1b8ab99b35ab6b0000a2119bfa70baf15cac15
nihil27011996 <pellissery.n@northeastern.edu>
2023-10-25 20:01:14 -0400

    changes to the code

--- packer/app.sh
+++ packer/app.sh
@@ -7,6 +7,8 @@ sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
 sudo mv /home/admin/webapp.zip /opt/csye6225/webapp.zip
 cd /opt/csye6225/
 sudo unzip webapp.zip
+sudo chown -R csye6225:csye6225 /opt/csye6225
+sudo chmod -R 750 /opt/csye6225
 sudo cp /opt/csye6225/packer/healthz-systemd.service /lib/systemd/system/healthz-systemd.service
 sudo npm install
 sudo apt-get remove -y git
--- packer/db.sh
+++ packer/db.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 # Installing the postresql sever
-sudo apt-get update
-sudo apt-get install -y postgresql postgresql-contrib
-
-# starting the server
-sudo systemctl start postgresql.service
-sudo systemctl enable postgresql.service
-
-# Set the password for the user
-sudo -u postgres psql -c "ALTER ROLE postgres WITH PASSWORD 'Assasinscreed2!';"
-
-
-# Stop the PostgreSQL service
-sudo systemctl stop postgresql.service
+# sudo apt-get update
+# sudo apt-get install -y postgresql postgresql-contrib
+# sudo systemctl start postgresql.service
+# sudo systemctl enable postgresql.service
+# sudo -u postgres psql -c "ALTER ROLE postgres WITH PASSWORD 'Assasinscreed2!';"
+# sudo systemctl stop postgresql.service
