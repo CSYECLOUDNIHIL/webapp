408aed2c43372475c0ec9860267bcdb1db3750f5
nihil27011996 <114561628+nihil27011996@users.noreply.github.com>
2023-11-06 15:30:34 -0500

    Fork feature branch (#74)
    * adding the configuration files
    
    * mades changes to config file
    
    * mades changes to config file
    
    * mades changes to config file
    
    * changes made to the code

--- config/logger.js
+++ config/logger.js
@@ -7,19 +7,7 @@ const logger = createLogger({
   level: 'info',
   format: format.json(), 
   defaultMeta: { service: 'csye6225' },
-  transports: [
-    new transports.Console(),
-    new transports.CloudWatch({
-      logGroupName: 'csye6225',
-      logStreamName: 'webapp',
-      createLogGroup: true,
-      createLogStream: true,
-      submissionInterval: 5000,
-      submissionRetryCount: 1,
-      batchSize: 20,
-      awsRegion: 'us-east-1',
-    }),
-  ],
+  transports: [new transports.File({ filename: '/var/log/csye6225.log' })],
 });
 
 module.exports = logger;
