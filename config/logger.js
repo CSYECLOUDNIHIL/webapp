2a78f5568c8a4b530d477d55baca86ec2a139fef
nihil27011996 <pellissery.n@northeastern.edu>
2023-11-06 19:46:36 -0500

    changes made to the code

--- config/logger.js
+++ config/logger.js
@@ -1,12 +1,13 @@
 const { createLogger, transports, format } = require('winston');
 const dotenv = require('dotenv');
+const appath=require('app-root-path');
 dotenv.config();
 
 const logger = createLogger({
   level: 'info',
   format: format.combine(format.timestamp(), format.json()),
   defaultMeta: { service: 'csye6225' },
-  transports: [new transports.File({ filename: '/logs/csye6225.log' })],
+  transports: [new transports.File({ filename: appath+'/logs/csye6225.log' })],
 });
 
 module.exports = logger;
